af298c4a12401bcfb81462a5a4c15a918d052a01
XenoHead <194586298+XenoHead@users.noreply.github.com>
2026-09-14 14:09:46 -0500

    run-nightly-enrichment

--- run-nightly-enrichment.bat
+++ run-nightly-enrichment.bat
@@ -3,3 +3,4 @@ echo Running Nightly Discogs Enrichment Batch...
 cd /d "c:\Git\ForeverYoung"
 call node enrich_inventory.js --remote --trial --limit 100
 echo Batch completed at %date% %time% >> enrichment_log.txt
+pause
